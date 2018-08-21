433832c0defc1267ff152a8e4eaab8f3870a21d7
Tim Biles <timbilestimbiles@gmail.com>
2018-08-21 16:52:44 -0500

    many style changes, added portfolio and contact page

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,70 @@ import Link from 'gatsby-link'
 
 import './header.css'
 
+console.log(window.location)
 const Header = ({ siteTitle }) => (
-  <div className="header_container">
-    <div className="header_main">
-      <Link className="header_link" to="/">
-        <h1>Deron Biles</h1>
-      </Link>
-    </div>
-    <div className="header_right">
-      <Link className="header_link" to="/">
-        Home
-      </Link>
+  <div>
+    {window.location.pathname === '/' ? (
+      <div
+        className="header_container"
+        style={{ backgroundColor: 'transparent' }}
+      >
+        <div className="header_main">
+          <Link className="header_link" to="/">
+            <h1>Deron Biles</h1>
+          </Link>
+        </div>
+        <div className="header_right">
+          <Link className="header_link" to="/">
+            Home
+          </Link>
 
-      <Link className="header_link" to="/about">
-        About
-      </Link>
+          <Link className="header_link" to="/about">
+            About
+          </Link>
 
-      <Link className="header_link" to="/sermons">
-        Sermons
-      </Link>
-    </div>
+          <Link className="header_link" to="/portfolio">
+            Portfolio
+          </Link>
+
+          <Link className="header_link" to="/contact">
+            Contact
+          </Link>
+        </div>
+      </div>
+    ) : (
+      <div
+        className="header_container"
+        style={{
+          backgroundColor: '#1B1A1B',
+          position: 'fixed',
+          width: '100%'
+        }}
+      >
+        <div className="header_main">
+          <Link className="header_link" to="/">
+            <h1>Deron Biles</h1>
+          </Link>
+        </div>
+        <div className="header_right">
+          <Link className="header_link" to="/">
+            Home
+          </Link>
+
+          <Link className="header_link" to="/about">
+            About
+          </Link>
+
+          <Link className="header_link" to="/portfolio">
+            Portfolio
+          </Link>
+
+          <Link className="header_link" to="/contact">
+            Contact
+          </Link>
+        </div>
+      </div>
+    )}
   </div>
 )
 
---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import './styles/contact.css'
+
+const Contact = () => (
+  <div className='contact'>
+    <div className='contact_container'>
+      <div className="bio_name_sub">
+        <div className="line_container">
+          <div className="a_line" />
+          <div className="a_line" />
+        </div>
+
+        <h1 className="about_name">Contact</h1>
+        <div className="line_container">
+          <div className="a_line" />
+          <div className="a_line" />
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Contact
